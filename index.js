db27ac48d0f89da634a7b2cc8c637b9222212e97
Crispus Nganga <githogoringash91@gmail.com>
2025-03-23 16:06:47 +0300

    added functions to index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'chris'
+function upperCaseCustomerName() {
+    customerName = 'BOB'
+    return 'BOB'
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return 'not bob'
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob'
+}
